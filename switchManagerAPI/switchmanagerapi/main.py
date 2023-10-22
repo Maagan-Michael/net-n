b66fd40798aa560857be81621b7aeb8dcfa20718
nathanschwarz <nathanschwarz@MacBook-Pro-de-nathan-2.local>
2023-10-22 11:19:18 +0300

    added cors policy for local dev

--- switchManagerAPI/switchmanagerapi/main.py
+++ switchManagerAPI/switchmanagerapi/main.py
@@ -1,6 +1,11 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from switchmanagerapi.routers import connections, customers, switches
+
+origins = [
+    "http://localhost:3000",
+]
 
 app = FastAPI(
     # todo : authentification dependency with external oauth service
@@ -8,10 +13,19 @@ app = FastAPI(
     responses={404: {"description": "Not found"}},
 )
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(connections.router)
 app.include_router(customers.router)
 app.include_router(switches.router)
 
+
 def main():
     """Run the API server"""
-    uvicorn.run("switchmanagerapi.main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("switchmanagerapi.main:app",
+                host="0.0.0.0", port=8000, reload=True)
