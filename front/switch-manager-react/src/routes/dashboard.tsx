c5fa786c090b3bf5b1f8a5d5763766197699a758
nathanschwarz <nathanschwarz@MacBook-Pro-de-nathan-2.local>
2023-10-23 10:08:57 +0300

    [front] starting search input

--- front/switch-manager-react/src/routes/dashboard.tsx
+++ front/switch-manager-react/src/routes/dashboard.tsx
@@ -1,3 +1,4 @@
+import clsx from "clsx";
 import { Outlet } from "react-router-dom";
 import { ReactComponent as Search } from "../components/icons/search.svg";
 
@@ -7,12 +8,12 @@ export default function Dashboard() {
       <section>
         <h1 className="font-thin text-3xl">SwitchManager</h1>
         <section>
-          <div className="rounded-md bg-neutral-100 w-[440px] mt-4 flex">
-            <Search className="w-12 h-12" />
+          <div className="rounded-md bg-neutral-100 w-[440px] mt-6 flex px-1 shadow">
+            <Search className={clsx("w-12 h-12", "text-red-400")} />
             <input
               type="search"
               placeholder="search..."
-              className="bg-transparent outline-none"
+              className="bg-transparent outline-none text-sm grow"
             />
           </div>
         </section>
