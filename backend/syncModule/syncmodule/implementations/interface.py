da99d510ee14e9e2e20c558e7eaa7e143c4c07fe
nathanschwarz <nathanschwarz@MacBook-Pro-de-nathan-2.local>
2023-11-26 15:35:52 +0200

    [syncModule]: architecture update; declared common interface for implementations

---
+++ backend/syncModule/syncmodule/implementations/interface.py
@@ -0,0 +1,151 @@
+from abc import abstractmethod
+from typing import Dict, List
+from pydantic import BaseModel
+
+from sqlalchemy import create_engine
+
+SourceDataType = Dict[
+    "customer": Dict[
+        "id": int,
+        "firstname": str,
+        "lastname": str,
+        "type": str,
+        "address": str
+    ],
+    "connection": Dict[
+        "toggled": bool,
+    ]
+]
+
+TargetDataType = Dict[
+    "customer": Dict[
+        "id": int,
+        "firstname": str,
+        "lastname": str,
+        "type": str,
+        "address": str
+    ],
+    "connection": Dict[
+        "id": int,
+        "toggled": bool,
+        "autoUpdate": bool
+    ]
+]
+
+
+class DBConfig(BaseModel):
+    """DBConfig is the configuration for the database"""
+    host: str
+    port: int
+    user: str
+    password: str
+    database: str
+    table: str
+
+    def generateUrl(self) -> str:
+        return f"postgresql+asyncpg://{self.user}:{self.password}@{self.host}/{self.database}"
+
+
+class ISyncModule:
+    """ISyncModule is the interface for the SyncModule"""
+
+    def __init__(self, destination: DBConfig):
+        self.destination = destination
+
+    @abstractmethod
+    def getSourceData(self) -> List[SourceDataType]:
+        """
+        gets the data from the source
+        this method should be implemented by the child class
+        this method should return a list of dict
+        SourceDataType is the type of the dict
+        """
+        pass
+
+    def getTargetData(self) -> List[TargetDataType]:
+        """gets the data from the target database"""
+        url = self.destination.generateUrl()
+        engine = create_engine(url)
+        columns = ",".join([
+            "customers.id",
+            "customers.firstname",
+            "customers.lastname",
+            "customers.type",
+            "customers.address",
+            "connections.toggled",
+            "connections.id",
+            "connections.autoUpdate"
+        ])
+        for x in columns:
+            x = {
+                "customer": {
+                    "id": x[0],
+                    "firstname": x[1],
+                    "lastname": x[2],
+                    "type": x[3],
+                    "address": x[4],
+                },
+                "connection": {
+                    "id": x[6],
+                    "toggled": x[5],
+                    "autoUpdate": x[7]
+                }
+            }
+        with engine.connect() as conn:
+            result = conn.execute(
+                f"""
+                SELECT {columns} FROM {self.destination.table}
+                FROM customers
+                LEFT JOIN connections ON = connections.customer_id
+                """
+            )
+        engine.dispose()
+        return result
+
+    def splitData(self) -> Dict[
+        "updates": Dict[
+            "customers": List[Dict],
+            "connections": List[Dict]
+        ],
+        "removes": Dict[
+            "customers": List[Dict],
+            "connections": List[Dict]
+        ]
+    ]:
+        """splits the data into updates and removes"""
+        sourceData = self.getSourceData()
+        targetData = self.getTargetData()
+        res = {
+            "updates": {
+                "customers": [],
+                "connections": []
+            },
+            "removes": {
+                "customers": [],
+                "connections": []
+            }
+        }
+        # getting customers and connections to update
+        for x in sourceData:
+            for (y, idx) in enumerate(targetData):
+                if x["customer"]["id"] == y["customer"]["id"]:
+                    if (x["customer"] != y["customer"]):
+                        res["updates"]["customers"].append(x)
+                    if (x["connection"]["toggled"] != y["connection"]["toggled"] and y["connection"]["autoUpdate"]):
+                        res["updates"]["connection"].append({
+                            "id": y["connection"]["id"],
+                            "toggled": x["connection"]["toggled"]
+                        })
+                    # remove from list
+                    del targetData[idx]
+                    break
+
+        # all the targetData left is to be removed only if autoUpdate is true
+        for (x, idx) in enumerate(targetData):
+            if x["connection"]["autoUpdate"]:
+                res["removes"]["customers"].append(x["customer"])
+                res["removes"]["connections"].append(x["connection"])
+
+    def sync(self):
+        """syncs the data from the source database to the target database"""
+        data = self.splitData()
---
+++ backend/syncModule/syncmodule/implementations/sqlSource.py
@@ -0,0 +1,69 @@
+from typing import Any, Dict, List
+from pydantic import BaseModel
+import logging
+from .interface import ISyncModule, DBConfig, TargetDataType
+from sqlalchemy import create_engine
+
+
+class SyncModuleConfig(BaseModel):
+    """
+        SyncModuleConfig is the configuration for the SyncModule
+        source: DBConfig is the source database configuration
+        destination: DBConfig is the destination database configuration
+        col_name_customer_id is the source column name for customer id
+        col_name_customer_firstname is the source column name for customer firstname
+        col_name_customer_lastname is the source column name for customer lastname
+        col_name_customer_type is the source column name for customer type
+        col_name_customer_address is the source column name for customer address
+        col_name_connection_toggled is the source column name for connection toggled
+    """
+    source: DBConfig
+    destination: DBConfig
+
+    col_name_customer_id: str
+    col_name_customer_firstname: str
+    col_name_customer_lastname: str
+    col_name_customer_type: str
+    col_name_customer_address: str
+    col_name_connection_toggled: str
+
+
+class SyncModule(ISyncModule):
+    """SyncModule is the module that syncs the data from the source database to the target database"""
+
+    def __init__(self, config: SyncModuleConfig):
+        super().__init__(config.destination)
+        self.config = config
+        self.logger = logging.getLogger('syncmodule')
+        self.logger.info('SyncModule init')
+
+    def getSourceData(self) -> List[TargetDataType]:
+        """gets the data from the source database"""
+        url = self.config.source.generateUrl()
+        engine = create_engine(url)
+        columns = ",".join([
+            self.config.col_name_customer_id,
+            self.config.col_name_customer_firstname,
+            self.config.col_name_customer_lastname,
+            self.config.col_name_customer_type,
+            self.config.col_name_customer_address,
+            self.config.col_name_connection_toggled
+        ])
+        for x in columns:
+            x = {
+                "customer": {
+                    "id": x[0],
+                    "firstname": x[1],
+                    "lastname": x[2],
+                    "type": x[3],
+                    "address": x[4],
+                },
+                "connection": {
+                    "toggled": x[5]
+                }
+            }
+        with engine.connect() as conn:
+            result = conn.execute(
+                f"SELECT {columns} FROM {self.config.source.table}")
+        engine.dispose()
+        return result
