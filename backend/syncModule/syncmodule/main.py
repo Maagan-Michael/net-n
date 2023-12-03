dc6d1611e068e3cc00f9429f75a16c0dc2d91a61
nathanschwarz <nathanschwarz@MacBook-Pro-de-nathan-2.local>
2023-12-03 10:00:48 +0200

    [syncModule]: added yaml and json config parser

--- backend/syncModule/syncmodule/main.py
+++ backend/syncModule/syncmodule/main.py
@@ -1,8 +1,20 @@
 import sys
 import json
+import yaml
 from .implementations.sqlSource import SQLSyncModule, SyncModuleConfig
 
 
+def getConfig(path: str) -> SyncModuleConfig:
+    with open(path, 'r') as f:
+        if (path.endswith(".yaml")):
+            return SyncModuleConfig(**yaml.load(f))
+        elif (path.endswith(".json")):
+            return SyncModuleConfig(**json.load(f))
+        else:
+            raise Exception("unknown file type")
+
+
 def __main__():
+    config = getConfig(sys.argv[1])
     print("to be implemented")
     pass
