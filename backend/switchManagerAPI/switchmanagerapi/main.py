136874e6638d077ee2923acacdf3b81f458e9bb0
nathanschwarz <nathanschwarz@MacBook-Pro-de-nathan-2.local>
2023-12-10 11:47:32 +0200

    [API]: documentation

--- backend/switchManagerAPI/switchmanagerapi/main.py
+++ backend/switchManagerAPI/switchmanagerapi/main.py
@@ -37,29 +37,53 @@ jobs = []
 
 @app.on_event("shutdown")
 async def on_shutdown():
-    """Run on shutdown"""
+    """
+    Run on shutdown
+    - cancel all running background jobs
+    """
     for job in jobs:
-        job.cancel()
+        if (not job.cancelled() and not job.done()):
+            job.cancel()
 
 
 @app.on_event("startup")
 async def on_startup():
-    """Run on startup"""
-    # pass
-    # await generateMockupDB()
-    # await drop_db()
-    await create_db()
-    # print(AppAdapter.adapter.getSwitchInterface("172.22.10.2", 630))
-    # print(AppAdapter.adapter.getSwitchInterface("10.100.64.251", 165))
-    # print(AppAdapter.adapter.getSwitchInterface("10.100.65.251", 702))
-    # AppAdapter.sync()
+    """
+        Run on startup
+        - create database
+        - start network adapter sync on startup
+        - start network adapter sync every 24 hours
+        - checks for due connections on startup
+        - checks for due connections every 12 hours
+    """
 
+    # drop database (for testing purposes)
+    # await drop_db()
+
+    # create database
+    await create_db()
+
+    # generate mockup database (for testing purposes)
+    # await generateMockupDB()
+
+    # start network adapter sync on startup
+    jobs.append(asyncio.create_task(AppAdapter.sync()))
+    # start network adapter sync every 24 hours
+    jobs.append(asyncio.create_task(
+        schedule_every(60 * 60 * 24, AppAdapter.sync))
+    )
+    # checks for due connections on startup
+    jobs.append(asyncio.create_task(toggleDueConnections()))
     # checks for due connections every 12 hours
     jobs.append(asyncio.create_task(
         schedule_every(60 * 60 * 12, toggleDueConnections)))
 
 
 def main():
-    """Run the API server"""
+    """
+        Run the API server
+        this is the entrypoint of the application when running in development
+        for production, run "uvicorn switchmanagerapi.main:app --host IP --port PORT"
+    """
     uvicorn.run("switchmanagerapi.main:app",
                 host="0.0.0.0", port=8000, reload=True)
