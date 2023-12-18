5a000d814c1b2145f21cef033a80eaa1bb23a99d
nathanschwarz <nathanschwarz@MacBook-Pro-de-nathan-2.local>
2023-12-18 21:53:20 +0200

    [frontend]: small animation for suspense loader

--- front/switch-manager-react/src/routes/dashboard/navigation/index.tsx
+++ front/switch-manager-react/src/routes/dashboard/navigation/index.tsx
@@ -44,7 +44,9 @@ export default function Navigation() {
           <button
             className={clsx(
               "absolute bottom-16 lg:top-0 cursor-pointer hover:text-blue-400 transition-all rounded-full bg-neutral-50 shadow-lg lg:shadow-md hover:shadow-lg flex items-center justify-center w-10 h-10",
-              "lg:end-4 right-4 lg:right-[unset]"
+              i18n.dir() === "rtl"
+                ? "right-4 lg:right-[unset] lg:left-4"
+                : "right-4"
             )}
             onClick={() => navigate(`/settings?${window.location.search}`)}
           >
