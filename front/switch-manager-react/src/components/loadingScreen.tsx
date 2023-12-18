e9486ac6f03c3ad63b80a5a25cc399ded99848a4
nathanschwarz <nathanschwarz@MacBook-Pro-de-nathan-2.local>
2023-12-18 21:46:43 +0200

    [frontend]: small animation for suspense loader

--- front/switch-manager-react/src/components/loadingScreen.tsx
+++ front/switch-manager-react/src/components/loadingScreen.tsx
@@ -1,9 +1,22 @@
+import { useTransition, animated } from "@react-spring/web";
+
 export default function LoadingScreen() {
-  return (
-    <div className="fixed top-0 left-0 w-screen h-screen flex items-center justify-center">
+  const [transitions, api] = useTransition(true, () => ({
+    from: { transform: "scale(1.0)" },
+    enter: { transform: "scale(1.0)", delay: 1000 },
+    leave: { transform: "scale(0.0)", delay: 5000 },
+    config: {
+      duration: 5000,
+    },
+  }));
+  return transitions((style) => (
+    <animated.div
+      className="fixed top-0 left-0 w-screen h-screen flex items-center justify-center"
+      style={style}
+    >
       <span className="font-thin text-xl md:text-3xl animate-pulse">
         SwitchManager
       </span>
-    </div>
-  );
+    </animated.div>
+  ));
 }
